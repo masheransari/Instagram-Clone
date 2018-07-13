2fb766d0b8a83544ef0da0f98425a1b52d3fb712
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2018-07-14 01:42:51 +0500

    Video 1:Header and bottom added in home

---
+++ InstagramClone/app/src/main/java/com/tech/universal/instagramclone/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.tech.universal.instagramclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
