bd7423f09d0dcdcf905f2d2e4287ee2d91e87bd4
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2018-07-14 15:00:48 +0500

    Tutorial 8: Added in which we are wokring in profile screen

--- InstagramClone/app/src/main/java/com/tech/universal/instagramclone/Profile/ProfileActivity.java
+++ InstagramClone/app/src/main/java/com/tech/universal/instagramclone/Profile/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.tech.universal.instagramclone.Profile;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,19 +19,46 @@ import com.tech.universal.instagramclone.R;
 public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "ProfileActivity";
     private static final int ACTIVITY_NUM = 4;
-    private Context mContext =ProfileActivity.this;
+    private Context mContext = ProfileActivity.this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         setupBottomNavigationView();
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: Click Menu item :" + item);
+
+                switch (item.getItemId()) {
+                    case R.id.profileMenu:
+                        break;
+
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+
+        return true;
     }
 
     /**
      * Bottom Naviation View Config Method
      */
     private void setupBottomNavigationView() {
-        BottomNavigationViewEx navigationViewEx=(BottomNavigationViewEx) findViewById(R.id.botttomNavViewBar);
+        BottomNavigationViewEx navigationViewEx = (BottomNavigationViewEx) findViewById(R.id.botttomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavView(navigationViewEx);
         BottomNavigationViewHelper.enableNavigation(mContext, navigationViewEx);
         Menu menu = navigationViewEx.getMenu();
